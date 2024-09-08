4f78f089b1900fcca6577d490c019cb2212e69b1
Josue <josuefiguer@gmail.com>
2024-09-07 21:07:37 -0600

    4ta modificando en cashitassss

--- registros-paros/src/App.js
+++ registros-paros/src/App.js
@@ -14,83 +14,13 @@ import Cashitas from './components/Cashitas';
 import './assets/css/componentes/area.css';
 import Frascos from './rutas/Frascos';
 import Salsas from './rutas/Salsas';
-import { useState } from 'react';
-import { v4 as uuid } from "uuid";
+//import { useState } from 'react';
+//import { v4 as uuid } from "uuid";
 import HojaDeParos from './components/HojaDeParos';
 
 
 function App() {
-  const [produccion, setProduccion] = useState([{
-    
-    id: uuid(),
-    foto: "frascos",
-    area: "FRASCOS",
-    descripcion: "Poner un descripcion"
-  },
-  {
-    id: uuid(),
-    foto: "salsas",
-    area: "SALSAS",
-    descripcion: "Poner un descripcion"
-  },
-  {
-    id: uuid(),
-    foto: "aceitunas",
-    area: "ACEITUNAS",
-    descripcion: "Poner un descripcion"
-  },
-  { 
-    id: uuid(),
-    foto: "masipack",
-    area: "MASIPACK",
-    descripcion: "Poner un descripcion"
-  },
-  {
-    id: uuid(),
-    foto: "frituras",
-    area: "FRITURAS",
-    descripcion: "Poner un descripcion"
-  }])
-
-  const [areas, setAreas] = useState ([
-   
-    { 
-      id: uuid(),
-      titulo: "FRASCOS",
-      colorPrimario: "#57C278",
-    },
-    {
-      id: uuid(),
-      titulo: "SALSAS",
-      colorPrimario: "#82CFFA"
-    },
-    {
-      id: uuid(),
-      titulo: "ACEITUNAS",
-      colorPrimario: "#A6D157"
-    },
-    {
-      id: uuid(),
-      titulo: "MASIPACK",
-      colorPrimario: "#E06B69"
-    },
-    {
-      id: uuid(),
-      titulo: "FRITURAS",
-      colorPrimario: "#DB6EBF"
-    },
-    {
-      id: uuid(),
-      titulo: "SOBRES",
-      colorPrimario: "#FFBA05",
-    },
-    {
-      id: uuid(),
-      titulo: "Innovación y Gestión",
-      colorPrimario: "#FF8A29"
-    }
-  ])
-    
+  
 
   return (
     <Router>
@@ -101,15 +31,7 @@ function App() {
       <Route path='/syso' element={<Syso />} />
       <Route path='/space' element={<Space />} />
       <Route path='/sasson' element={<Sasson />} />
-      <Route path='/cashitas' element={produccion.map((producto) => <Cashitas 
-       datos={producto}
-       key ={producto.area}
-       descripcion ={producto.descripcion}
-       foto ={producto.foto}
-       areas = {areas.filter(area => area.titulo === producto.descripcion)}
-      />) 
-      } 
-      />
+      <Route path='/cashitas' element={<Cashitas />} />
       <Route path='/frascos' element={<Frascos />} />
       <Route path='/salsas' element={<Salsas />} />
       <Route path='/hojaparos' element={<HojaDeParos />} />
--- registros-paros/src/components/Cashitas.jsx
+++ registros-paros/src/components/Cashitas.jsx
@@ -1,32 +1,101 @@
+import '../assets/css/componentes/area.css';
+import frasos from '../assets/img/frascos3.jpg';
+import salsas1 from '../assets/img/salsas2.jpg';
+import aceituna from '../assets/img/aceituna1.jpg';
+import sobre1 from '../assets/img/sobres1.jpg';
+import sobre2 from '../assets/img/sp2.jpg';
 import { Link } from "react-router-dom"
+import { AiOutlineLeftCircle } from "react-icons/ai";
 
-const Cashitas = (props) => {
-  const { area, descripcion, foto, id } = props.datos
-console.log(" ARRAY", props.datos)
+
+
+const Cashitas = () => {
+
   return <>
-  
 
-    <section className='colaboradores equipo'>
-      <div className="colaborador">
-      <h2 className="title-page">{area}</h2>
-        <div className="encabezado">
-          <Link to="/aceitunas" >
-            <img className='logos' src={foto} alt={area} />
-          </Link>
-        </div>
-        <div className="info">
-          <h4>{area}</h4>
-          <h5>{descripcion}</h5>
-        </div>
+  <main>
+      <div className="container">
+          <h2 className="title-page">CASHITAS</h2>
       </div>
-    </section>
+  </main>
+
+  <section className='colaboradores equipo'>
+      <div className="colaborador">
+          <div className="encabezado">
+              <Link to="/frascos" >
+                  <img className='logos' src={frasos} alt={"Frascos"} />
+              </Link>
+          </div>
+          <div className="info">
+              <h4>{"MASIPACK 1"}</h4>
+              <h5>{"Linea Automatizada"}</h5>
+          </div>
+      </div>
+
+      <div className="colaborador">
+          <div className="encabezado">
+              <Link to="/salsas">
+                  <img className='logos' src={salsas1} alt={"Salsas"} />
+              </Link>
+          </div>
+          <div className="info">
+              <h4>{"MASIPACK 2"}</h4>
+              <h5>{"DAMOS SABOR A TU VIDA"}</h5>
+          </div>
+      </div>
+      <div className="colaborador">
+          <div className="encabezado">
+              <Link to="/aceitunas">
+                  <img className='logos' src={aceituna} alt={"Aceitunas"} />
+              </Link>
+          </div>
+          <div className="info">
+              <h4>{"MASIPACK 3"}</h4>
+              <h5>{"DAMOS SABOR A TU VIDA"}</h5>
+          </div>
+      </div>
+      <div className="colaborador">
+          <div className="encabezado">
+              <Link to="/sobres1">
+                  <img className='logos' src={sobre1} alt={"SP1"} />
+              </Link>
+          </div>
+          <div className="info">
+              <h4>{"BIRESPACK 1"}</h4>
+              <h5>{"DAMOS SABOR A TU VIDA"}</h5>
+          </div>
+      </div>
+      <div className="colaborador">
+          <div className="encabezado">
+              <Link to="/sobres2">
+                  <img className='logos' src={sobre2} alt={"SP2"} />
+              </Link>
+          </div>
+          <div className="info">
+              <h4>{"BIRESPACK 2"}</h4>
+              <h5>{"DAMOS SABOR A TU VIDA"}</h5>
+          </div>
+      </div>
+      <div className="colaborador">
+          <div className="encabezado">
+              <Link to="/relleno">
+                  <img className='logos' src={salsas1} alt={"RELLENO"} />
+              </Link>
+          </div>
+          <div className="info">
+              <h4>{"BIRESPACK 3"}</h4>
+              <h5>{"DAMOS SABOR A TU VIDA"}</h5>
+          </div>
+      </div>
+      <div className='regresar'>
+          <Link to="/" >
+              <AiOutlineLeftCircle className='margen_flecha'/>
+          </Link>
+      </div>
+  </section>
 
 
-
-
-  </>
-
-
+</>
 
 }
 
